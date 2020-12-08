2656cdfbcda33800e10250cb5dffb1932bac281b
biccariluca <73994645+biccariluca@users.noreply.github.com>
2020-12-08 18:31:09 +0100

    tolto ;
    ;

--- BiccariMain.java
+++ BiccariMain.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 public class BiccariMain extends Application{
 	public static void main (String[]args){
-		Application.launch(args);
+		Application.launch(args)
 	}
 	//
 	public void start(Stage stage) throws IOException{
